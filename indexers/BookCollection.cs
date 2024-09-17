c37bf5cd0fdd8cf20d2586129010c4c67f72cd17
Abdullah Hebish <Abdullahhebish0@gmail.com>
2024-09-17 22:57:23 +0300

    first commit

---
+++ indexers/BookCollection.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace indexers
+{
+    internal class BookCollection
+    {
+        private Book[] books = new Book[3];
+
+		public Book this[int indexer]
+		{
+			get 
+			{
+                return books[indexer];
+			}
+
+			set { books[indexer] = value; }
+		}
+        public Book this[string title]
+        {
+            get
+            {
+                foreach (var item in books)
+                {
+                    if (item.Title == title)
+                    {
+                        return item;
+                    }
+                }
+
+                return null;
+            }
+
+
+            set {
+
+                    for (var i = 0; i < books.Length; i++)
+                    {
+                        if (books[i].Title == title)
+                        {
+                            books[i] = value;
+                            return;
+                        }
+                    }
+                    throw new InvalidOperationException("Book not found");
+                }
+        }
+    }
+}
---
+++ indexers/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace indexers
+{
+    public class program
+    {
+        static void Main(string[] args)
+        {
+            BookCollection book = new BookCollection();
+            book[0]=new Book() { Author = "Author1",Title = "Free Palestine"};
+            book[1] = new Book() { Author = "Author2", Title = "Free soudan" };
+            Console.WriteLine(book[0].Title);
+            Console.WriteLine(book["Free Palestine"].Author);
+        }
+    }
+   
+}
